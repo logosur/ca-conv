967eeb9f796ad20869df1fca65dc9517246d573b
Iván López <ivan@codeholics.io>
2023-06-20 14:07:59 +0200

    set right fixtures
    php bin/console doctrine:fixtures:load --purge-exclusions="App\Entity\User" --append

--- backend/src/DataFixtures/AppFixtures.php
+++ backend/src/DataFixtures/AppFixtures.php
@@ -7,17 +7,24 @@ use App\Entity\Ciudad;
 use App\Entity\TiposProducto;
 use App\Entity\ProductoFinanciero;
 use App\Entity\Pregunta;
+use App\Entity\Recomendacion;
 use App\Entity\UsuariosRespuesta;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+use DateTime;
 
 class AppFixtures extends Fixture
 {
+    private User $user;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $usuarios = $em->getRepository(User::class)->findAll();
+        $this->user = $usuarios[0];
+    }
     public function load(ObjectManager $manager): void
     {
-        $usuarios = $manager->getRepository(User::class)->findAll();
-        dump($usuarios);
-        exit("aki");
         $ciudad = new Ciudad();
         $ciudad->setCiudad('Sevilla');
         $manager->persist($ciudad);
@@ -47,12 +54,19 @@ class AppFixtures extends Fixture
         $productoFinanciero->setBeneficiosAdicionales(50.31);
         $manager->persist($productoFinanciero);
 
-        $usuario = $manager->getRepository(User::class)->find(1);
-
         $usuariosRespuesta = new UsuariosRespuesta();
         $usuariosRespuesta->setPregunta($pregunta);
-        $usuariosRespuesta->setUsuario($usuario[0]);
+        $usuariosRespuesta->setUsuario($this->user);
+        $usuariosRespuesta->setPregunta($pregunta);
+        $usuariosRespuesta->setRespuesta("Respuesta 1");
+        $usuariosRespuesta->setFecha(new DateTime());
+        $manager->persist($usuariosRespuesta);
 
+        $recomendacion = new Recomendacion();
+        $recomendacion->setUsuario($this->user);
+        $recomendacion->setProducto($productoFinanciero);
+        $recomendacion->setFecha(new DateTime());
+        $manager->persist($recomendacion);
 
         $manager->flush();
     }
